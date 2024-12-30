51f7f92f92f6e84deb68bf3c9820f240932952d1
Shuichiro Sakata <sakashu.10.22@gmail.com>
2024-12-31 04:11:29 +0900

    study routings of express

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.post('/', function (req, res) {
+    res.send('Got a POST request');
+})
+
+app.put('/user', function (req, res) {
+    res.send('Got a PUT request at /user');
+})
+
+app.delete('/user', function (req, res) {
+    res.send('Got a DELETE request at /user');
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
